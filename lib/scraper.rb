d73301b974cbc0c8f7af8c91bda97a0d49a72066
Shasha Yan <shashayan@gmail.com>
2021-05-19 21:50:19 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,9 @@
 require 'nokogiri'
 require 'open-uri'
 
-html = open("https://flatironschool.com/")
+doc = Nokogiri::HTML(open("https://flatironschool.com/"))
+doc.css(".headline-26OIBN")
+
+doc.css(".headline-26OIBN").text
+
+puts doc.css(".headline-26OIBN")
