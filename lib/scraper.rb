24f7b7db413b8970f769f88f793ac9ac6a5f7b4a
Shasha Yan <shashayan@gmail.com>
2021-05-19 20:55:20 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,3 +2,7 @@ require 'nokogiri'
 require 'open-uri'
 
 html = open("https://flatironschool.com/")
+Nokogiri::HTML(html)
+doc = Nokogiri::HTML(html)
+
+doc = Nokogiri::HTML(open("https://flatironschool.com/"))
